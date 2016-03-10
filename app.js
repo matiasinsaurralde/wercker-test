85a18f385777ac6be516eb91d46cca0994fef5ef
Matias Insaurralde <matias@insaurral.de>
2016-03-10 05:54:25 -0300

    env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = {}
 var redis = require("redis"),
-    client = redis.createClient()
+    client = redis.createClient( process.env.REDIS_PORT_6379_TCP_PORT, process.env.REDIS_PORT_6379_TCP_ADDR )
 app.doStuff = function() {
   client.set( 'abc', '123' )
 }
